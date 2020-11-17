46b908da6d6df50c13d367efc9ff32425b559d4a
Jeff Han <37327453+rainboww3@users.noreply.github.com>
2020-11-17 20:01:06 +0900

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-cd ~ 
-mkdir local
-mkdir ~/repos
-cd ~/repos
-git clone https://github.com/mirror/ncurses.git
-cd ncurses
-./configure --prefix ~/local --enable-widec --with-pthread
-make -j
-make -j install
-
-
 cd ~
 
 make game
